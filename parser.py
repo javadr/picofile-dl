7e6bcef18d91952fe9673eeab5aafb5d8c8bc6c0
javad <javadr@gmail.com>
2021-11-06 08:44:42 +0330

    Parser Update; Nov 6, 2021

--- parser.py
+++ parser.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-
 import configargparse as argparse
 import sys
 import tempfile
@@ -14,7 +13,7 @@ def parse_args(args=None):
     """
 
     parse_kwargs = {
-        "description":  'Download a file/bunch of files from picofile server.'
+        "description": 'Download a file/bunch of files from picofile server.'
     }
     parser = argparse.ArgParser(**parse_kwargs)
 
@@ -29,48 +28,46 @@ def parse_args(args=None):
         default=None,
         help='a specific url that you want to download from picofile')
 
-    group_basic.add_argument(
-        '-f',
-        '--filename',
-        dest='filename',
-        action='store',
-        default=None,
-        help='a file includes the picofile urls')
+    group_basic.add_argument('-f',
+                             '--filename',
+                             dest='filename',
+                             action='store',
+                             default=None,
+                             help='a file includes the picofile urls')
 
     group_basic.add_argument(
-        '-p', 
+        '-p',
         '--path',
         dest='path',
         action='store',
         default=f"{tempfile.gettempdir()}/picofile-dl",
         help='path to save dowloaded files (Default: /<TEMP>/picofile-dl)')
 
-    parser.add_argument(
-        '-v', 
-        '--version',
-        dest='version',
-        action='store_true',
-        default=False,
-        help='display version and exit')
+    parser.add_argument('-v',
+                        '--version',
+                        dest='version',
+                        action='store_true',
+                        default=False,
+                        help='display version and exit')
 
-    parser.add_argument(
-        '--verbose',
-        dest='verbose',
-        action='store_true',
-        default=False,
-        help='display more info')
+    parser.add_argument('--verbose',
+                        dest='verbose',
+                        action='store_true',
+                        default=False,
+                        help='display more info')
 
     # Final parsing of the options
     args = parser.parse_args(args)
 
     # show version?
     if args.version:
-        print(f"Picofile-dl Ver. {info.__version__}, released on {info.__date__}")
+        print(
+            f"Picofile-dl Ver. {info.__version__}, released on {info.__date__}"
+        )
         sys.exit(0)
 
-    if not args.url and not args.filename:
+    if (args.url and not args.filename) or (not args.url and args.filename):
         parser.print_usage()
         sys.exit(1)
-
 
     return args
