14cedc5842a54b4c60bba0531383bb50234b37f8
javad <javadr@gmail.com>
2023-07-20 07:53:31 +0330

    (style) clean code

--- parsers.py
+++ parsers.py
@@ -1,67 +1,64 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
 
-import configargparse as argparse
 import sys
 import tempfile
+
+import configargparse as argparse
 import info
 
 
 def parse_args(args=None):
-    """
-    Parse the arguments/options passed to the program on the command line.
-    """
-
+    """Parse the arguments/options passed to the program on the command line."""
     parse_kwargs = {
-        "description": 'Download a file/bunch of files from picofile server.'
+        "description": "Download a file/bunch of files from picofile server.",
     }
     parser = argparse.ArgParser(**parse_kwargs)
 
     # Basic options
-    group_basic = parser.add_argument_group('Basic options')
+    group_basic = parser.add_argument_group("Basic options")
 
     group_basic.add_argument(
-        '-u',
-        '--url',
-        dest='url',
-        action='store',
+        "-u",
+        "--url",
+        dest="url",
+        action="store",
         default=None,
-        help='a specific URL that you want to download from picofile')
+        help="a specific URL that you want to download from picofile")
 
-    group_basic.add_argument('-f',
-                            '--filename',
-                            dest='filename',
-                            action='store',
+    group_basic.add_argument("-f",
+                            "--filename",
+                            dest="filename",
+                            action="store",
                             default=None,
-                            help='a file includes the picofile URLs')
+                            help="a file includes the picofile URLs")
 
     group_basic.add_argument(
-        '--password',
-        dest='password',
-        action='store',
+        "--password",
+        dest="password",
+        action="store",
         default=123456,
-        help='password set with the picofile URL')
+        help="password set with the picofile URL")
 
     group_basic.add_argument(
-        '-p',
-        '--path',
-        dest='path',
-        action='store',
+        "-p",
+        "--path",
+        dest="path",
+        action="store",
         default=f"{tempfile.gettempdir()}/picofile-dl",
-        help='path to save dowloaded files (Default: /<TEMP>/picofile-dl)')
+        help="path to save dowloaded files (Default: /<TEMP>/picofile-dl)")
 
-    parser.add_argument('-v',
-                        '--version',
-                        dest='version',
-                        action='store_true',
+    parser.add_argument("-v",
+                        "--version",
+                        dest="version",
+                        action="store_true",
                         default=False,
-                        help='display version and exit')
+                        help="display version and exit")
 
-    parser.add_argument('--verbose',
-                        dest='verbose',
-                        action='store_true',
+    parser.add_argument("--verbose",
+                        dest="verbose",
+                        action="store_true",
                         default=False,
-                        help='display more info')
+                        help="display more info")
 
     # Final parsing of the options
     args = parser.parse_args(args)
@@ -69,7 +66,7 @@ def parse_args(args=None):
     # show version?
     if args.version:
         print(
-            f"Picofile-dl Ver. {info.__version__}, released on {info.__date__}"
+            f"Picofile-dl Ver. {info.__version__}, released on {info.__date__}",
         )
         sys.exit(0)
 
--- picofile-dl.py
+++ picofile-dl.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 # getting files from picofile.com
 
-from selenium.webdriver import Firefox
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.common import exceptions
-import os, time
+import os
 import subprocess
-from timebudget import timebudget
-#timebudget.set_quiet()  # don't show measurements as they happen
+import time
+import urllib
+from pathlib import Path
+
+import seedir as sd
+from parsers import parse_args
+from rich import print
 
 from rich.progress import track
-from rich import print
-import seedir as sd
-from pathlib import Path
-import urllib
-
-from parsers import parse_args
-
+from selenium.common import exceptions
+from selenium.webdriver import Firefox
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from timebudget import timebudget
+#timebudget.set_quiet()  # don't show measurements as they happen
 
 def getDownloadLink(driver, url, password):
     driver.get(url)
@@ -26,25 +25,22 @@ def getDownloadLink(driver, url, password):
         driver.find_element(By.ID, "filePassword").send_keys(password)
     except exceptions.NoSuchElementException:
         pass
-    elem = driver.find_element(By.ID, 'getDownloadLink')
+    elem = driver.find_element(By.ID, "getDownloadLink")
     elem.click()
     time.sleep(2)
-    elem = driver.find_element(By.ID, 'downloadLink')
+    elem = driver.find_element(By.ID, "downloadLink")
     return elem.get_attribute("href")
 
 
 @timebudget
 def picofile_dl():
-    """
-    Main entry point for execution.
-    """
-
+    """Main entry point for execution."""  # noqa: D401
     args = parse_args()
     driver = Firefox()
 
     if args.filename:
         with open(args.filename) as fu:
-            urls = [item.replace('\n', '').strip() for item in fu.readlines()]
+            urls = [item.replace("\n", "").strip() for item in fu.readlines()]
     elif args.url:
         urls = [args.url]
     downloadPath = Path(args.path).absolute()
@@ -54,47 +50,51 @@ def picofile_dl():
     try:
         for i, url in enumerate(
                 track(urls, description="[green]Downloading...[/green]"), 1):
-            showurl = urllib.parse.unquote(f'{i}/{len(urls)}: {url}')
+            showurl = urllib.parse.unquote(f"{i}/{len(urls)}: {url}")
             try:
                 href = getDownloadLink(driver, url, args.password)
                 if not href:
-                    print(f'{url} [blink][red]does not fetch![/red][/blink]')
+                    print(f"{url} [blink][red]does not fetch![/red][/blink]")
                     continue
             except exceptions.NoSuchElementException:
-                print(f'{showurl} [blink][red]is expired![/red][/blink]')
+                print(f"{showurl} [blink][red]is expired![/red][/blink]")
                 continue
             except exceptions.WebDriverException as exp:
-                print(showurl, exp.msg, sep='\n')
+                print(showurl, exp.msg, sep="\n")
                 continue
 
-            filename = urllib.parse.unquote(href[href.rfind('/') + 1:])
+            filename = urllib.parse.unquote(href[href.rfind("/") + 1:])
             # picofile converts `-` to `_` in the download url
-            if list(downloadPath.glob(filename.replace('_', '*'))):
+            if list(downloadPath.glob(filename.replace("_", "*"))):
                 print(f"{showurl} already exists!")
                 continue
 
             print(showurl)
 
             #cmd = ['axel', '-Ncvn4', href]
-            cmd = ['wget', '-qnd', '--no-check-certificate', href]
-            proc = subprocess.Popen(cmd,
-                                    stdout=subprocess.PIPE,
-                                    stderr=subprocess.PIPE)
+            cmd = ["wget", "-qnd", "--no-check-certificate", href]
+            proc = subprocess.Popen(
+                cmd,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+            )
             o, e = proc.communicate()
 
             try:
-                print("    Output: " + o.decode('ascii').split('\n')[-2])
+                print("    Output: " + o.decode("ascii").split("\n")[-2])
             except:
                 print("    Output: [err: ascii decode] ", o)
-            if e: print(f"    Error: {e.decode('ascii')}")
-            if proc.returncode: print(f"    code: {str(proc.returncode)}")
+            if e:
+                print(f"    Error: {e.decode('ascii')}")
+            if proc.returncode:
+                print(f"    code: {str(proc.returncode)}")
 
-        print(f" DONE! ({args.path}) ".center(70, '='))
+        print(f" DONE! ({args.path}) ".center(70, "="))
         sd.seedir(args.path)  # print tree directory
         timebudget.report()
     finally:
         driver.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     picofile_dl()
